a0047d1e44e4b1a39b402dc8f3302f740348c6df
Uzzi72 <46907234+Uzzi72@users.noreply.github.com>
2020-04-19 22:28:22 -0400

    Update web_crawl_example1.py

--- A12/web_crawl_example1.py
+++ A12/web_crawl_example1.py
@@ -4,10 +4,14 @@ import requests
 URL= 'https://www.monster.com/jobs/search/?q=Programmer&where=Tennessee'
 page= requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
+results = soup.find(id='ResultsContainer')     #print(results_collapse1.prettify())
 
 job_elems = results.find_all('section', class_='card-content')
 for job_elem in job_elems:
+
+    #each job element is a new beautifulsoup object
+    #You can use the same methods on it as you did before
+     
     title_elem = job_elem.find('h2', class_='title')
     company_elem = job_elem.find('div', class_='company')
     location_elem = job_elem.find('div', class_='location')
